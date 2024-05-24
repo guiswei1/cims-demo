9a1e5ff5a662c267ea5531b70846f3b1d336cde8
shuangweigui@gmail.com <shuangweigui@gmail.com>
2024-05-24 08:40:33 +0800

    init

--- js/main.js
+++
@@ -1,8 +0,0 @@
-var MessageBody = require('./JT808Server_pb');
-var JT1078MessageBody = require('./JT1078Server_pb');
-var ResMessageBody = require('./JT808TRspServer_pb');
-module.exports = {
-    DataProto: MessageBody,
-    JT1078DataProto: JT1078MessageBody,
-    ResDataProto: ResMessageBody
-}
